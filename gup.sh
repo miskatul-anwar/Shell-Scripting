252edf01e3cf8bad4fdb97919eb9ef1a667112ff
Miskatul Anwar <miskatul.anwar.csecu@gmail.com>
2024-03-18 01:42:03 +0000

    Mon Mar 18 01:42:03 AM UTC 2024

--- gup.sh
+++ gup.sh
@@ -4,13 +4,15 @@ a=$(date)
 b=$(pwd)
 lim=$(tput cols)
 for ((i = 0; i <= $lim; i++)); do
-	echo "="
+	printf "="
 done
+printf "\n"
 git add .
 git commit -m "$a"
 git push origin main
 for ((i = 0; i <= $lim; i++)); do
-	echo "="
+	printf "="
 done
+printf "\n"
 echo "Process:: $b --> main" | lolcat
 echo "Successfull! 🤠 "
